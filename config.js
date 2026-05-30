89dd48ae21d0027dc687c495517ed5a8aa3320fd
silasalice583 <silasalice583@gmail.com>
2026-05-30 22:29:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Cloudflare Pages 部署后，把这里改成 Railway 后端域名，例如：
 // window.SIRIUS_API_BASE = "https://sirius-portal-api.up.railway.app";
-window.SIRIUS_API_BASE = "";
+window.SIRIUS_API_BASE = "sirius-portal-site-production.up.railway.app";
